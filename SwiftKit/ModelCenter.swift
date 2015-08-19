c980cb081b8dd2e38ad840e31442fc84b5e72b45
宋文通 <275712575@qq.com>
2015-08-19 11:22:31 +0800

    调整结构

---
+++ SwiftKit/ModelCenter.swift
@@ -0,0 +1,13 @@
+//
+//  ModelCenter.swift
+//  SwiftKit
+//
+//  Created by SongWentong on 8/19/15.
+//  Copyright © 2015 QuantGroup. All rights reserved.
+//
+
+import UIKit
+
+class ModelCenter: NSObject {
+
+}
